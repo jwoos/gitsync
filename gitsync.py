ce9d3d5e91ca46d18f721056fa584d122ae78a26
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-03-21 18:52:12 -0400

    initial python file

---
+++ gitsync.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import argparse
+
+config = {
+    'username': '',
+    'token': '',
+    'debug': False,
+    'connection': '',
+    'verbose': ''
+}
+
+
+def main():
+    pass
